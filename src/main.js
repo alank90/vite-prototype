8003a2ad606a20913fc22d90cf525f627802e9b7
Alan K <akillian@outlook.com>
2024-01-12 10:14:55 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+/* add line if using vue router, if not delete
+import router from "./router/index.js";*/
 
 createApp(App).mount('#app')
+
+/* Replace previous line with this if using vue router
+createApp(App).use(router).mount("#app");*/
+
