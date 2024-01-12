f2cbadde40397385c81c341816243bbc50464dc1
Alan K <akillian@outlook.com>
2024-01-12 10:29:35 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <a href="https://vuejs.org/" target="_blank">
     <img src="./assets/logo.png" class="logo vue" alt="Vue logo" />
   </a>
