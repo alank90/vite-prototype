31634a5303d83fdd2b5d232c0690ce21a7294f66
Alan K <akillian@outlook.com>
2024-01-12 11:17:25 -0500

    Add semi-colons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 /* add line if using vue router, if not delete
 import router from "./router/index.js";*/
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
 
 /* Replace previous line with this if using vue router
 createApp(App).use(router).mount("#app");*/
-
