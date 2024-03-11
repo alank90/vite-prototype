513836fe6c5f1ba272680b0963475b9cd8c2e016
Alan K <akillian@outlook.com>
2024-03-11 12:53:50 -0400

    Update .eslintrc.cjs
    Remove @stlystic/eslint-plugin-js entry from plugins array.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,9 +8,8 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: "module",
   },
-  plugins: ["vue","@stylistic/js"],
+  plugins: ["vue"],
   rules: {
-    "@stylistic/js/semi": ["error","always"],
     "vue/no-multiple-template-root": "off",
     "vue/multi-word-component-names": [
       "error",
